cfa85d05674829aa72dfaede8679e30b4a428212
Joshua Mason <jmmason95@googlemail.com>
2026-09-24 17:24:50 +0100

    mac-headroom 0.6.0

--- Formula/mac-headroom.rb
+++ Formula/mac-headroom.rb
@@ -1,8 +1,8 @@
 class MacHeadroom < Formula
   desc "Find where macOS disk space went and clear caches safely"
   homepage "https://github.com/joshua-mason/mac-headroom"
-  url "https://github.com/joshua-mason/mac-headroom/archive/refs/tags/v0.5.0.tar.gz"
-  sha256 "b7731de6b3ac8af2bc310f37ca4499011eb5a1ad7367c1fea72b4eb8d3c31fe4"
+  url "https://github.com/joshua-mason/mac-headroom/archive/refs/tags/v0.6.0.tar.gz"
+  sha256 "52e782fe9a7b48ff680bd18d0da30c53104938752b9da391c60baa2b6b289f02"
   license "MIT"
   head "https://github.com/joshua-mason/mac-headroom.git", branch: "main"
 
